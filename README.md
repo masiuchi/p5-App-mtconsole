8a447f791b292992f70785e8ca440266ebb697c6
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2017-02-28 18:34:10 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::mtconsole - Console for Movable Type
+
+# SYNOPSIS
+
+    $ mtconsole
+
+# DESCRIPTION
+
+App::mtconsole is a console for Movable Type like rails console.
+
+# LICENSE
+
+Copyright (C) Masahiro Iuchi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Masahiro Iuchi <masahiro.iuchi@gmail.com>
---
+++ lib/App/mtconsole.pm
@@ -0,0 +1,39 @@
+package App::mtconsole;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::mtconsole - Console for Movable Type
+
+=head1 SYNOPSIS
+
+    $ mtconsole
+
+=head1 DESCRIPTION
+
+App::mtconsole is a console for Movable Type like rails console.
+
+=head1 LICENSE
+
+Copyright (C) Masahiro Iuchi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Masahiro Iuchi E<lt>masahiro.iuchi@gmail.comE<gt>
+
+=cut
+
